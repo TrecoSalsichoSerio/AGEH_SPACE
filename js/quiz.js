58a70ebb764f948ba83c738ceae3fcda53c8b8f5
antonytonio <137085892+antonytonio@users.noreply.github.com>
2024-11-04 10:56:05 -0300

    Add files via upload

--- js/quiz.js
+++ js/quiz.js
@@ -1,4 +1,266 @@
-document.getElementById("enviarQuiz").addEventListener("click", verificarRespostas);
+// MONTAGEM DAS QUESTOES
+const questoes = [
+    {
+        title: "Qual o planeta mais distante do Sol?",
+        alternativas: [
+            {
+                name: "Plutão",
+                value: "0"
+            },
+            {
+                name: "Vênus",
+                value: "0"
+            },
+            {
+                name: "Marte",
+                value: "0"
+            },
+            {
+                name: "Netuno",
+                value: "1"
+            },  
+        ]
+    },
+    {
+        title: "Qual planeta foi rebaixado a anão?",
+        alternativas: [
+            {
+                name: "Plutão",
+                value: "1"
+            },
+            {
+                name: "Saturno",
+                value: "0"
+            },
+            { 
+                name: "Jupter",
+                value: "0"
+            },
+            {
+                name: " Terra",
+                value: "0"
+            },  
+        ]
+    },
+    {
+        title: "Qual a temperatura aproximada da superfície do Sol?",
+        alternativas: [
+            {
+                name: "6700 °C",
+                value: "0"
+            },
+            {
+                name: "9800 °C",
+                value: "0"
+            },
+            {
+                name: "5500 °C",
+                value: "1"
+            },
+            {
+                name: "2100 °C",
+                value: "0"
+            },  
+        ]
+    },
+    {
+        title: "Qual o menor planeta do noso sistema solar?",
+        alternativas: [
+            {
+                name: "Urano",
+                value: "0"
+            },
+            {
+                name: "Venus",
+                value: "0"
+            },
+            {
+                name: "Mercúrio",
+                value: "1"
+            },
+            {
+                name: "Marte",
+                value: "0"
+            },  
+        ]
+    },
+    {
+        title: "Qual são os dois principais componentes químicos do Sol?",
+        alternativas: [
+            {
+                name: "Hélio e Hidrogênio",
+                value: "1"
+            },
+            {
+                name: "Ferro e Níquel",
+                value: "0"
+            },
+            {
+                name: "Gás Carbônico e Hidrogênio",
+                value: "0"
+            },
+            {
+                name: "Oxigênio e Níquel",
+                value: "0"
+            },  
+        ]
+    },
+    {
+        title: "Quantos planetas, atualmente, tem no sistema Solar?",
+        alternativas: [
+            {
+                name: "10",
+                value: "0"
+            },
+            {
+                name: "8",
+                value: "1"
+            },
+            {
+                name: "9",
+                value: "0"
+            },
+            {
+                name: "7",
+                value: "0"
+            },  
+        ]
+    },
+    {
+        title: "Quantas luas tem Saturno?",
+        alternativas: [
+            {
+                name: "112",
+                value: "0"
+            },
+            {
+                name: "236",
+                value: "0"
+            },
+            {
+                name: "50",
+                value: "0"
+            },
+            {
+                name: "145",
+                value: "1"
+            },  
+        ]
+    },
+    {
+        title: "Qual o nome dos dois satélites de Marte?",
+        alternativas: [
+            {
+                name: "Ganimedes e Fobos",
+                value: "0"
+            },
+            {
+                name: "Deimos e Ganimedes",
+                value: "0"
+            },
+            {
+                name: "Fobos e Deimos",
+                value: "1"
+            },
+            {
+                name: "Fobos e Titãn    ",
+                value: "0"
+            },  
+        ]
+    },
+    {
+        title: "Qual o planeta mais próximo do sol?",
+        alternativas: [
+            {
+                name: "Mercúrio",
+                value: "1"
+            },
+            {
+                name: "Venus",
+                value: "0"
+            },
+            {
+                name: "Marte",
+                value: "0"
+            },
+            {
+                name: "Saturno",
+                value: "0"
+            },  
+        ]
+    },
+    {
+        title: "Qual o maior planeta do sistema solar?",
+        alternativas: [
+            {
+                name: "Terra",
+                value: "0"
+            },
+            {
+                name: "Saturno",
+                value: "0"
+            },
+            {
+                name: "Júpter",
+                value: "1"
+            },
+            {
+                name: "Marte",
+                value: "0"
+            },  
+        ]
+    },
+]
+
+
+// CRIACAO DAS QUESTOES
+window.addEventListener("DOMContentLoaded", ()=>{
+
+    let contador = 1
+
+    questoes.forEach(questao=>{
+        const div_questao = document.createElement('div')
+        div_questao.classList.add('questao')
+
+        const p_texto = document.createElement('p')
+        p_texto.textContent = questao.title
+
+        div_questao.appendChild(p_texto)
+
+        questao.alternativas.forEach(alternativa=>{
+            const label = document.createElement('label')
+            label.classList.add('mabel')
+
+            const input = document.createElement('input')
+            input.value = alternativa.value
+            input.name = "q" + contador
+            input.type = "radio"
+            
+            label.appendChild(input)
+            label.innerHTML += " - "+alternativa.name
+            
+            div_questao.append(label)
+        })
+        
+        contador ++
+        document.querySelector(".quests").append(div_questao)
+        console.log(div_questao)
+    })
+
+
+
+
+
+
+
+
+        
+    document.getElementById("enviarQuiz").addEventListener("click", verificarRespostas);
+})
+
+
+
+// ORGANIZACAO DAS QUESTOES E PONTUACAO
 
 function verificarRespostas(){
     document.querySelectorAll(".questao").forEach(div=>{
