e311aa52d0b6aa4c184449fd7c36c8a4fc9ce91b
GoginGamePlaysFullHD1080p <137086342+GoginGamePlaysFullHD1080p@users.noreply.github.com>
2024-11-05 02:05:08 -0300

    Add files via upload

--- js/info.js
+++ js/info.js
@@ -1,3 +1,20 @@
+paginaAtual = 0
+
 function visualizar(pagina){
-    document.body.setAttribute('page', pagina)
+
+    if(pagina == 'Voltar'){
+        paginaAtual = paginaAtual - 1
+    }
+    else if(pagina == 'Avançar'){
+        paginaAtual = paginaAtual + 1
+    }
+
+    if(paginaAtual < 0){
+        paginaAtual = 0
+    }
+    else if(paginaAtual > pagMaximo){
+        paginaAtual = pagMaximo
+    }
+
+    document.body.setAttribute('page', `pag${paginaAtual}`)
 }
